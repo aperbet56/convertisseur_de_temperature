3405552fcf07f4369c56ab27beda677b029889c5
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-09-24 18:39:54 +0200

    Renommage des fonctions

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 const celsiusInput = document.querySelector("#celsuis");
 const fahrenheitInput = document.querySelector("#fahrenheit");
 
-//  Fonction celToFar qui va permettre de transformer les celsuis en fahrenheit
-const celToFar = () => {
+// Déclaration de la fonction celsuisToFahrenheit qui va permettre de transformer les celsuis en fahrenheit
+const celsuisToFahrenheit = () => {
   let fahrenheit = (parseFloat(celsiusInput.value) * 9) / 5 + 32;
   fahrenheitInput.value = parseFloat(fahrenheit.toFixed(2));
   console.log(fahrenheit);
 };
 
-// Ecoute de l'événement input sur le champs celsuis et appel de la fonction celToFar
-celsiusInput.addEventListener("input", celToFar);
+// Ecoute de l'événement input sur le champs celsuis et appel de la fonction celsuisToFahrenheit
+celsiusInput.addEventListener("input", celsuisToFahrenheit);
 
-// Fonction farToCel qui va permettre de transformer les fahrenheit en celsius
-const farToCel = () => {
+// Déclaration de la fonction fahrenheitToCelsuis qui va permettre de transformer les fahrenheit en celsius
+const fahrenheitToCelsuis = () => {
   let celsius = ((parseFloat(fahrenheitInput.value) - 32) * 5) / 9;
   celsiusInput.value = parseFloat(celsius.toFixed(2));
   console.log(celsius);
 };
 
-// Ecoute de l'événement input sur le champs fahrenheit et appel de la fonction farToCel
-fahrenheitInput.addEventListener("input", farToCel);
+// Ecoute de l'événement input sur le champs fahrenheit et appel de la fonction fahrenheitToCelsuis
+fahrenheitInput.addEventListener("input", fahrenheitToCelsuis);
